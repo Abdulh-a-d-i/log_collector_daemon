81f5a99913f6258589a60385d29499f6d758cda7
Abdul Hadi <abdulhadizaeem7@gmail.com>
2025-11-04 23:27:33 +0500

    Refactor system_info.py to include additional metrics

--- system_info.py
+++ system_info.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-# system_info.py
 import platform
 import psutil
 import socket
 import uuid
 import json
-import os
+from datetime import datetime
 
 def get_system_info():
     uname = platform.uname()
-    svmem = psutil.virtual_memory()
     disk = psutil.disk_usage('/')
+    svmem = psutil.virtual_memory()
     ip = None
     for iface, addrs in psutil.net_if_addrs().items():
         for a in addrs:
@@ -19,26 +18,19 @@ def get_system_info():
                 break
         if ip:
             break
-
     return {
+        "collected_at": datetime.utcnow().isoformat() + "Z",
+        "hostname": socket.gethostname(),
+        "ip": ip or "NotFound",
         "os": uname.system,
         "os_version": uname.version,
-        "os_release": uname.release,
-        "hostname": socket.gethostname(),
-        "architecture": uname.machine,
-        "mac": ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0, 8 * 6, 8)][::-1]),
-        "ip": ip or "NotFound",
-        "disk_gb": {
-            "total": round(disk.total / (1024**3), 2),
-            "used": round(disk.used / (1024**3), 2),
-            "free": round(disk.free / (1024**3), 2),
-            "percent": disk.percent
-        },
+        "arch": uname.machine,
+        "mac": ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0,8*6,8)][::-1]),
+        "disk_total_gb": round(disk.total / (1024**3), 2),
+        "disk_used_gb": round(disk.used / (1024**3), 2),
         "ram_gb": round(svmem.total / (1024**3), 2),
-        "cpu": {
-            "physical": psutil.cpu_count(logical=False),
-            "logical": psutil.cpu_count(logical=True)
-        }
+        "cpu_physical": psutil.cpu_count(logical=False),
+        "cpu_logical": psutil.cpu_count(logical=True)
     }
 
 if __name__ == "__main__":
